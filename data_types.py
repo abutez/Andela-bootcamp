a8b621d186ebc045dac46f0d70b9959593a37d36
abutez <steveabuta@gmail.com>
2016-10-31 23:50:51 +0300

    updated function for various data types

--- data_types.py
+++ data_types.py
@@ -1,13 +1,13 @@
-
+# function to test various data types
 def data_type(n):
-
+# condititonal statements for string,boolean, lists and integer
   if isinstance(n,str):
     return len(n)
-  elif isinstance(n,type(none)):
+  elif isinstance(n,type(None)):
       return"no value"
   elif isinstance(n,bool):
             return (n)
-  elif isinstance(n, int):
+  elif isinstance(n,int):
             if n<100:
               return "less than 100"
             elif n>100:
@@ -16,9 +16,7 @@ def data_type(n):
               return"equal to 100"
 
   elif isinstance(n,list):
-    if len(n)>=[3]:
-        return n [2]
-    else:
+    if len(n)>=3:
         return n[2]
-
-print data_type("haloo")
+    else:
+        return None
