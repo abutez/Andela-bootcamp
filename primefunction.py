6e790d09aa47da96ad876585dd3be5777b3542fa
abutez <steveabuta@gmail.com>
2016-10-31 22:36:31 +0300

    prime numbers functions updated

--- primefunction.py
+++ primefunction.py
@@ -1,21 +1,16 @@
-
+# a function to identify prime numbers and print out the list
 def primes(number):
+# an if statement to check weather a given integer is prime or not
     if number>1:
         for numb in range(2, number):
-
             if number%numb==0:
-
                 return False
             else:
                 continue
         return True
     else:
          return False
-
-
-
-
-
+# function to print a list of prime numbers
 def primes_list(number):
     our_primes=[]
     for numb in range(2,number+1):
